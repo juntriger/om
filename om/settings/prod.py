5eb8455f1f6590161dba1ca30a1cbdd667fccc10
juntriger <juntriger@gmail.com>
2021-08-09 10:27:14 +0700

    change apply PostgreSQL

--- om/settings/prod.py
+++ om/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['18.142.86.108', 'bsvproduction.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'om',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '+rZ#Gj86nv;`JM)%bxF:,r2a9!EzYgCK',
+        'HOST': 'ls-c10cd64e6234e3777557784bcf18989ef78c04c0.clgqotdraprx.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
